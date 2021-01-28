a3f43d3e9e06a858c4c181f41347fc43183ff9cd
Sora513 <kokubo.tech@gmail.com>
2021-01-28 22:27:24 +0900

    navbar custom

--- script.js
+++ script.js
@@ -42,13 +42,11 @@ $(function () {
                     short[i] = courselist_short[i];
                     for_split[i] = String(20) + for_split[i].replace(/-/g, "");
                     container = courselist[i].split(for_split[i]);
-                    console.log(short[i])
                     if (container.length == 1) {
                         //特殊なクラス(時間割じゃないコース)
                         term[i] = "none"
                         name[i] = container[0]
                         time[i] = "none"
-                        console.log(container[0])
                     } else {
                         //通常クラス
                         name[i] = container[0]
@@ -67,16 +65,12 @@ $(function () {
                     courses[i].time = time[i]
                     courses[i].url = url[i]
                 }
-                console.log(courses);
 
 
                 //ナビゲーション文字入れ替え
                 var listnum = $(".depth_1 ul").first().children("li").eq(2).children("ul").children("li").length
                 var count = 0;
                 $(".depth_1 ul").first().children("li").last().children("ul").children("li").each(function () {
-                    console.log($(this).children("p").children("a"));
-
-                    console.log(listnum)
                     var tf = false;
                     count++
                     for (var i = 0; i < coursenum; i++) {
@@ -98,21 +92,54 @@ $(function () {
                 });
 
                 //ストレージに保持(local->syncで他拡張機能と共有可能?)
-
                 chrome.storage.local.set({"courses":courses},function(){
                 })
+
+                //myコースの表示設定消去
+                $(".block_mycourse_config").remove()
+
+                //header消去
+                $("header").empty().css("height","50px")
+
+                //navバー操作
+                $("nav").prepend("<p>Hello Moodle</p>")
+
+                //navを左に集める＆順番最適化
+                $("#page-header").after("<div id=\"side-nav-extension\"></div>")
+                var side_nav_extensions_css={
+                    "width": "360px",
+                    "margin": "0px 0 0 0"
+                }
+                $("#side-nav-extension").css(side_nav_extensions_css)
+                $("#side-nav-extension").append($(".columnleft").html())
+                $(".columnleft").remove()
+                $("#side-nav-extension").append($(".columnright").html())
+                $(".columnright").remove()
+
+                var search_course=$("[data-block=\"html\"]").last()
+                var jyouhou_security=$("[data-block=\"html\"]").first()
+                var navigator=$("[data-block=\"navigation\"]")
+                var mysyllabus=$("[data-block=\"mysyllabus\"]")
+                var private_files=$("[data-block=\"private_files\"]")
+                var calendar_upcoming=$("[data-block=\"calendar_upcoming\"]")
+                var badges=$("[data-block=\"badges\"]")
+                var calendar_month=$("[data-block=\"calendar_month\"]")
+
+                $("#block-region-side-pre").empty()
+                $("#block-region-side-post").remove()
+                $("#block-region-side-pre").append(jyouhou_security,calendar_month,calendar_upcoming,navigator,search_course,mysyllabus,private_files,badges)
+
 
             }
         }, 100)
     }else{
         //topページ以外での処理
         chrome.storage.local.get("courses",function(data){
-            console.log(data.courses)
             var coursenum=data.courses.length
                             //ナビゲーション文字入れ替え
                             var listnum = $(".depth_1 ul").first().children("li").eq(2).children("ul").children("li").length
                             var count = 0;
-                            console.log(listnum)
+        
                             $(".depth_1 ul").first().children("li").eq(2).children("ul").children("li").each(function () {
             
                                 var tf = false;
