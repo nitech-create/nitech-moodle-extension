96113b15876e82edbf2a33b5d25465da80682ee4
smk7758 <smk7758@users.noreply.github.com>
2021-02-09 14:30:44 +0900

    optionsへの移管と修正
    popupからoptionsへ移管するなど。

--- options/options.js
+++ options/options.js
@@ -1,21 +1,74 @@
-let backgroundColor = document.getElementById('backgroundColor');
+let pageElements = {
+  backgroundColorInput: document.getElementById("backgroundColor"),
+  invisibleLeftNavigationOnlyVideo: document.getElementById("invisibleLeftNavigationOnlyVideo")
+};
 
-// storageから現在の値を取得して、表示を変更
-chrome.storage.local.get('backgroundColor', function(data) {
-  if (data.backgroundColor == undefined) {
-    data.backgroundColor = "NavajoWhite";
-  }
-  backgroundColor.value = data.backgroundColor;
-});
+let defaultOptions = {
+  backgroundColor: "NavajoWhite",
+  invisibleLeftNavigationOnlyVideo: true
+};
 
-function save() {
-  console.log(backgroundColor.value);
-  chrome.storage.local.set({"backgroundColor": backgroundColor.value});
+// 関数定義
+function setOptionsFromStorage() {
+  // storageから現在の値を取得して、表示を変更
+  // TODO: よりよい関数名, もともとはloadStorageOptions. 時間がかかりそうなイメージがほしい
+
+  chrome.storage.local.get("backgroundColor", function(data) {
+    if (data.backgroundColor == undefined) {
+      data.backgroundColor = defaultOptions.backgroundColor;
+    }
+    console.log("storage: " + data.backgroundColor);
+    pageElements.backgroundColorInput.value = data.backgroundColor;
+  });
+
+  chrome.storage.local.get("invisibleLeftNavigationOnlyVideo", function(data) {
+    if (data.invisibleLeftNavigationOnlyVideo == undefined) {
+      data.invisibleLeftNavigationOnlyVideo = defaultOptions.invisibleLeftNavigationOnlyVideo;
+    }
+    invisibleLeftNavigationOnlyVideo.checked = data.invisibleLeftNavigationOnlyVideo;
+  });
 }
 
-// saveボタンを押したときにstorageへ保管
-document.getElementById('save').addEventListener('click', function () {
-  save();
+function setOptions(options) {
+  // TODO: 関数名にToPageをつけるか？
+  console.log("setPage: " + options.backgroundColor);
+  pageElements.backgroundColorInput.value = options.backgroundColor;
+  return options;
+}
+
+function loadPageOptions() {
+  return loadedOptions = {
+    backgroundColor: pageElements.backgroundColorInput.value,
+    invisibleLeftNavigationOnlyVideo: pageElements.invisibleLeftNavigationOnlyVideo.checked
+  };
+}
+
+function saveToStorage(options) {
+  console.log("save: "+options);
+  chrome.storage.local.set(options); //TODO
+  // chrome.storage.local.set({"backgroundColor": options.backgroundColor});
+  // chrome.storage.local.set({"backgroundColor": backgroundColor.value});
+}
+
+// page load時.start
+console.log("default: " + defaultOptions.backgroundColor);
+setOptionsFromStorage();
+// page load時.finish
+
+// Pageのボタンなどの設定.start
+document.getElementById("save").addEventListener("click", function () {
+  saveToStorage(loadPageOptions());
 });
 
-// TODO: reset butoon
+// loadDefault
+document.getElementById("loadDefault").addEventListener("click", function () {
+  setOptions(defaultOptions);
+  // saveToStorage(setOptions(defaultOptions));
+});
+
+// loadCurrent
+document.getElementById("loadCurrent").addEventListener("click", function () {
+  setOptionsFromStorage();
+  // saveToStorage(setOptions(defaultOptions));
+});
+// Pageのボタンなどの設定.finish
--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,8 @@
 // Optionsページを開くボタン
-document.getElementById('openOptions').addEventListener("click", function() {
+document.getElementById("openOptions").addEventListener("click", function() {
     if (chrome.runtime.openOptionsPage) {
       chrome.runtime.openOptionsPage();
     } else {
-      window.open(chrome.runtime.getURL('options.html'));
+      window.open(chrome.runtime.getURL("options.html"));
     }
 });
-
-// invisibleLeftNavigationOnlyVideo
-// popup.htmlのボタンの取得
-let invisibleLeftNavigationOnlyVideo = document.getElementById('invisibleLeftNavigationOnlyVideo');
-
-// storageから現在の値を取得して、表示を変更
-chrome.storage.local.get('invisibleLeftNavigationOnlyVideo', function(data) {
-    invisibleLeftNavigationOnlyVideo.checked = data.invisibleLeftNavigationOnlyVideo;
-});
-
-// ボタンを押した時
-invisibleLeftNavigationOnlyVideo.onclick = function(element) {
-    chrome.storage.local.set({'invisibleLeftNavigationOnlyVideo': element.target.checked}, function() {
-        console.log("popup>invisibleLeftNavigationOnlyVideo: " + element.target.checked);
-    })
-};
