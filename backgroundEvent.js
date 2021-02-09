1debfe0e3da36b82b55242acc64588d36ad79971
smk7758 <smk7758@users.noreply.github.com>
2021-02-09 18:58:40 +0900

    add onLoad, loadJson
    拡張機能をloadした時に、Jsonを読み込めるようにしました。
    ただし、options機能で今後処理を追加します。

--- backgroundEvent.js
+++ backgroundEvent.js
@@ -1,23 +1,39 @@
+function loadJson(filePath, callback) {
+  chrome.runtime.getPackageDirectoryEntry(function (root) {
+    // get file
+    root.getFile(filePath, { create: false }, function (sample) {
+      // callback
+      sample.file(function (file) {
+        // read file
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.addEventListener('load', function (e) {
+          // parse and return json
+          // const response = {
+          //   url: chrome.extension.getURL('data'),
+          //   settings: JSON.parse(e.target.result),
+          // };
+
+          const settingsJson = JSON.parse(e.target.result);
+          callback(settingsJson, chrome.extension.getURL('data'));
+        });
+      });
+    });
+  });
+}
+
 // 拡張機能がインストールされたときの処理
 chrome.runtime.onInstalled.addListener(function () {
-  let config = {};
-  {
-    const file = 'config.json';
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', chrome.extension.getURL(file), true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-        config = JSON.parse(xhr.responseText);
-      }
-    };
-    xhr.send();
-  }
+  console.log('onLoad');
+  loadJson('defaultOptions.json', settingsJson => {
+    // TODO: load and save to storage (options.js).
 
-  {
-    for (key in config) {
-      if (typeof config[key] === 'string' && config[key] != '') {
-        console.log('key=' + key + ', value=' + config[key]);
+    // test
+    console.log('settingsJson', settingsJson);
+    for (key in settingsJson) {
+      if (typeof settingsJson[key] === 'string' && settingsJson[key] != '') {
+        console.log('key=' + key + ', value=' + settingsJson[key]);
       }
     }
-  }
+  });
 });
