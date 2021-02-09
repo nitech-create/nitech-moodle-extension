04da44576cb8b5bcd353e098675670ed780ca0c9
smk7758 <smk7758@users.noreply.github.com>
2021-02-09 21:44:22 +0900

    Revert "カレンダー部分を分離"
    This reverts commit a0ac4ae1a4f861f1d09a26b05e5510f1870ace23.

--- script.js
+++ script.js
@@ -551,9 +551,14 @@ $(function () {
             }
           }, 1000);
 
-          // external file; ./miniCalender/miniCalender.js
-          editCalender(calendar_month)
-
+          // カレンダーがうまく動かない(first.jsのcalendar_miniとか、calendar_get_monthなんちゃらとかが関係してるけど、ちょっと読めない(miniのほうが大事そう)) ->issue立てて隠ぺいしよう!(人任せ)
+          $('.arrow').css('visibility', 'hidden');
+          $('.arrow_link').css('cursor', 'default');
+          calendar_month
+            .children('div')
+            .append(
+              '<br><a id="link-to-calendar" href="">カレンダーに移動する</a>',
+            );
           $('#link-to-calendar').attr(
             'href',
             $('.current').eq(1).children('a').attr('href'),
