4e2ae05b037866cd8ef50c003133934b644eab5f
smk7758 <smk7758@users.noreply.github.com>
2021-02-09 22:17:58 +0900

    add requests
    backgroundEvent.js:
    accessStorageとaccessOptionsの追加。

--- backgroundEvent.js
+++ backgroundEvent.js
@@ -1,3 +1,7 @@
+let defaultOptions;
+// onInstalled: 拡張機能がインストールされたときの処理
+chrome.runtime.onInstalled.addListener(onLoad);
+
 function loadJson(filePath, callback) {
   chrome.runtime.getPackageDirectoryEntry(function (root) {
     // get file
@@ -22,17 +26,19 @@ function loadJson(filePath, callback) {
   });
 }
 
-// Listener処理
+// request Listener処理
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log('sender: ', sender);
-  if (request.item == 'defaultOptions')
-    sendResponse({ defaultOptions: defaultOptions });
+  switch (request.item) {
+    case 'defaultOptions':
+      sendResponse({ defaultOptions: defaultOptions });
+      BroadcastChannel;
+    case 'accessStorage':
+      sendResponse({ accessStorage: accessStorage });
+    case 'accessOptions':
+      sendResponse({ accessOptions: accessOptions });
+  }
 });
-
-let defaultOptions;
-
-// onInstalled: 拡張機能がインストールされたときの処理
-chrome.runtime.onInstalled.addListener(onLoad);
 
 function onLoad() {
   console.log('onLoad');
@@ -61,13 +67,59 @@ function onLoad() {
   });
 }
 
-// eslint-disable-next-line no-unused-vars
-function clearStorage() {
-  chrome.storage.local.clear(function () {
-    const error = chrome.runtime.lastError;
-    if (error) {
-      console.error(error);
+const accessOptions = {
+  // eslint-disable-next-line no-unused-vars
+  loadOptions: async function loadOptions(options) {
+    // storageから現在の設定を取得
+
+    try {
+      options.backgroundColor = await getStorage('backgroundColor');
+      options.hideNavOnVideo = await getStorage('hideNavOnVideo');
+    } catch (e) {
+      console.log(e);
     }
-  });
-  onLoad();
-}
+  },
+
+  saveOptions: function saveOptions(options) {
+    // storageにデータを保存
+    console.log('save: ', options);
+    chrome.storage.local.set(options); // TODO
+    // chrome.storage.local.set({"backgroundColor": options.backgroundColor});
+    // chrome.storage.local.set({"backgroundColor": backgroundColor.value});
+  },
+};
+
+// eslint-disable-next-line no-unused-vars
+const accessStorage = {
+  // eslint-disable-next-line no-unused-vars
+  getStorage: function getStorage(key) {
+    // chrome.storage.local.getのPromiseラッパー
+
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(key, function (data) {
+        if (!data.hasOwnProperty(key)) {
+          // ストレージにキーが存在しない
+          if (defaultOptions.hasOwnProperty(key)) {
+            console.log('loading default option of ' + key);
+            resolve(defaultOptions[key]);
+          } else {
+            reject(new Error('undefined key: ' + key));
+          }
+        } else {
+          resolve(data[key]);
+        }
+      });
+    });
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  clearStorage: function clearStorage() {
+    chrome.storage.local.clear(function () {
+      const error = chrome.runtime.lastError;
+      if (error) {
+        console.error(error);
+      }
+    });
+    onLoad();
+  },
+};
