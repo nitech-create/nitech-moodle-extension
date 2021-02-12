069dfe05ffa8ed7b77dc043cef623fc359a4beef
smk7758 <smk7758@users.noreply.github.com>
2021-02-13 01:24:53 +0900

    refactoring: convertCourses(←convertAndLoadCourses)
    ・convertCoursesの作成(←convertAndLoadCourses)
    ・loadCourselistの作成
    
    2役だったので修正した。
    coursesの内部変数名が変更されているため注意。

--- main.js
+++ main.js
@@ -117,7 +117,9 @@ async function reformTopPage(courseSize) {
   // 読み込み終わったらの処理
   // todolistの作成(取得?)
 
-  const courses = convertAndLoadCourses(courseSize);
+  const courselist_short = $('.course-listitem .text-muted div').text().slice(1).split('|'); // 取得してきたcourseの要素達
+  const courses = convertCourses(loadCourselist(), courselist_short, courseSize);
+  console.log(courses);
 
   // ストレージに保持(local->syncで他拡張機能と共有可能?)
   // awaitする必要はない
@@ -455,85 +457,77 @@ function moveNaviToLeft() {
   $('.columnright').remove();
 }
 
-// TODO: 関数名
 /**
- * DOMからコースの情報courselist, courselist_short(取得してきたcourseの要素達)を抜いて、courseに変換する。
+ * courselist:
+ * (授業名)(courseShortNumber)(前/後)期(月/...)曜(n-n')限_cls
+ * SpecialCourseはcourseShortNumberが無い
  *
- * @param {int} courseSize
- * @return {Array} courses
+ * @return {Array} courselist
  */
-function convertAndLoadCourses(courseSize) {
-  const courses = new Array(courseSize);
-  const courselist_short = $('.course-listitem .text-muted div').text().slice(1).split('|');
-
+function loadCourselist() {
   const courselist = $('.course-listitem .coursename')
     .text()
     .replace(/\s+/g, '')
     .split('コース星付きコース名');
   courselist.shift();
 
-  console.log($('.course-listitem .coursename').first().attr('href'));
+  return courselist;
+}
 
-  const short = new Array(courseSize);
+/**
+ * 取得してきたcourseの要素達から変換し、coursesを生成する。
+ *
+ * @param {Array} courseList: 通常コース: (授業名)(courseShortNumber)(前/後)期(月/...)曜(n-n')限_cls, 特殊コースはSpecialCourseはcourseShortNumberが無い。
+ * @param {String} courseNumberTxtList: 授業番号表記(-あり)。 (-なしはshort付き)
+ * @param {int} courseSize
+ * @return {Object} courses = {term, name, dayOfWeeks, times, url, courseNumberTxt}
+ */
+function convertCourses(courseList, courseNumberTxtList, courseSize) {
+  const courses = new Array(courseSize); // result
+
+  // 変数名がわかりづらいかもしれない
   const term = new Array(courseSize);
-  const day = new Array(courseSize);
   const name = new Array(courseSize);
-  const time = new Array(courseSize);
+  const dayOfWeeks = new Array(courseSize);
+  const times = new Array(courseSize);
   const url = new Array(courseSize);
-
   for (let i = 0; i < courseSize; i++) {
-    short[i] = courselist_short[i]; // TODO: !?
-    courselist_short[i] = String(20) + courselist_short[i].replace(/-/g, ''); // constなのに！？ <- 配列なので書き換えできる
+    const shortCourseNumberItem = String(20) + courseNumberTxtList[i].replace(/-/g, ''); // -を消去し西暦と授業番号の組み合わせ、固有な値: 202010001 など
+    const courseContainerArray = courseList[i]
+      .split(new RegExp(shortCourseNumberItem + '|期|曜|限|_cls'))
+      .filter(value => {
+        return value != '';
+      });
+    // courseContainerArray: [授業名, (前/後), (月/...), (n-n'), ((月/...), (n-n'),) ...] ※複数時間に注意
 
-    const courseContainerArray = courselist[i].split(courselist_short[i]);
-    // ["授業名", "(前/後)期(月/...)曜(n-n')限_cls"]
-    // TODO:
-    console.log('courseContainer0: ', courseContainerArray);
+    name[i] = courseContainerArray[0];
+    url[i] = $('.course-listitem .coursename').eq(i).attr('href');
 
     if (courseContainerArray.length == 1) {
       // 特殊なクラス(時間割じゃないコース)
-      // 'none'ではなく「nilでもnullでもundefinedでもfalse」←ここらへんにしたい気がする。
+      // TODO: 'none'ではなく「nilでもnullでもundefinedでもfalse」←ここらへんにしたい気がする。
       term[i] = 'none';
-      name[i] = courseContainerArray[0];
-      time[i] = 'none';
-      url[i] = $('.course-listitem .coursename').eq(i).attr('href');
+      times[i] = 'none';
     } else {
       // 通常クラス
-      name[i] = courseContainerArray[0];
+      term[i] = courseContainerArray[1];
 
-      // TODO: ここ絶対キレイに書ける
-      courseContainerArray[1] = courseContainerArray[1].split('期');
-      console.log('courseContainer[1] ', courseContainerArray[1]);
-      term[i] = courseContainerArray[1].shift();
-
-      courseContainerArray[1] = courseContainerArray[1][0].split('曜');
-      console.log('here');
-      day[i] = [];
-      // let countdaynum = 0;
-      for (let j = 0; j < courseContainerArray[1].length - 1; j++) {
-        day[i].push(courseContainerArray[1][0]);
-        console.log(courseContainerArray[1]);
-        // countdaynum++;
+      // 週複数授業の曜日と時間(限)
+      dayOfWeeks[i] = [];
+      times[i] = [];
+      for (let j = 2; j < courseContainerArray.length; j += 2) {
+        dayOfWeeks[i].push(courseContainerArray[j]);
+        times[i].push(courseContainerArray[j + 1]);
       }
-      console.log(day[i]);
-      // courseContainerArray[1].splice(0,countdaynum)
-      // 上のコメントアウトを外し下の行を消去するといろいろ止まるので、まとまった時間のある時に修正 --Sora513
-      day[i] = courseContainerArray[1].shift();
-
-      console.log(courseContainerArray[1]);
-      courseContainerArray[1] = courseContainerArray[1][0].split('限');
-      time[i] = courseContainerArray[1].shift();
-
-      url[i] = $('.course-listitem .coursename').eq(i).attr('href');
     }
 
     courses[i] = {
       term: term[i],
       name: name[i],
-      day: day[i],
-      short: short[i],
-      time: time[i],
+      dayOfWeeks: dayOfWeeks[i],
+      times: times[i],
       url: url[i],
+      courseNumberTxt: courseNumberTxtList[i], // ここだけ妙に見えるかもしれないが、わかりづらくて申し訳ない。
     };
   }
   return courses;
@@ -644,16 +638,16 @@ async function drawCourses(courses, nowTerm, nowDayOfWeek, todolist) {
   }
 }
 
-// eslint-disable-next-line no-unused-vars
+// TODO: 一週間表示
 function drawWeekTimeSchedule(courses) {
-  console.log('一週間表示');
-  $('body').append('<div id="overlay_extension"></div>');
-  $('head').append(
-    '<style>#overlay_extension::-webkit-scrollbar{width: 10px;}#overlay_extension::-webkit-scrollbar-track{background: #fff;border: none;border-radius: 10px;box-shadow: inset 0 0 2px #777;}#overlay_extension::-webkit-scrollbar-thumb{background: #ccc;border-radius: 10px;box-shadow: none;}</style>',
-  );
-  $('#overlay_extension').append(
-    '<table style="border-collapse: collapse" id="classtable_extension_overlay"><tr><th></th><th>月曜</th><th>火曜</th><th>水曜</th><th>木曜</th><th>金曜</th></tr><tr><td style="height:90px">1限<br>8：50～9：35</td><td rowspan="2" id="onegen_extension_overlay"></td></tr><tr><td style="height:90px">2限<br>9：35～10：20</td></tr><tr><td style="height:20px">休憩<br>10：20～10：30</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">3限<br>10：30～11：15</td><td rowspan="2" id="threegen_extension_overlay"></td></tr><tr><td style="height:90px">4限<br>11：15～12：00</td></tr><tr><td style="height:120px">昼休み<br>12：00～13：00</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">5限<br>13：00～13：45</td><td rowspan="2" id="fivegen_extension_overlay"></td></tr><tr><td style="height:90px">6限<br>13：45～14：30</td></tr><tr><td style="height:20px">休憩<br>14：30～14：40</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">7限<br>14：40～15：25</td><td rowspan="2" id="sevengen_extension_overlay"></td></tr><tr><td style="height:90px">8限<br>15：25～16：10</td></tr><tr><td style="height:20px">休憩<br>16：10～60：20</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">9限<br>16：20～17：05</td><td rowspan="2" id="ninegen_extension_overlay"></td></tr><tr><td style="height:90px">10限<br>17：05～17：50</td></tr></table>',
-  );
+  // console.log('一週間表示');
+  // $('body').append('<div id="overlay_extension"></div>');
+  // $('head').append(
+  //   '<style>#overlay_extension::-webkit-scrollbar{width: 10px;}#overlay_extension::-webkit-scrollbar-track{background: #fff;border: none;border-radius: 10px;box-shadow: inset 0 0 2px #777;}#overlay_extension::-webkit-scrollbar-thumb{background: #ccc;border-radius: 10px;box-shadow: none;}</style>',
+  // );
+  // $('#overlay_extension').append(
+  //   '<table style="border-collapse: collapse" id="classtable_extension_overlay"><tr><th></th><th>月曜</th><th>火曜</th><th>水曜</th><th>木曜</th><th>金曜</th></tr><tr><td style="height:90px">1限<br>8：50～9：35</td><td rowspan="2" id="onegen_extension_overlay"></td></tr><tr><td style="height:90px">2限<br>9：35～10：20</td></tr><tr><td style="height:20px">休憩<br>10：20～10：30</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">3限<br>10：30～11：15</td><td rowspan="2" id="threegen_extension_overlay"></td></tr><tr><td style="height:90px">4限<br>11：15～12：00</td></tr><tr><td style="height:120px">昼休み<br>12：00～13：00</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">5限<br>13：00～13：45</td><td rowspan="2" id="fivegen_extension_overlay"></td></tr><tr><td style="height:90px">6限<br>13：45～14：30</td></tr><tr><td style="height:20px">休憩<br>14：30～14：40</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">7限<br>14：40～15：25</td><td rowspan="2" id="sevengen_extension_overlay"></td></tr><tr><td style="height:90px">8限<br>15：25～16：10</td></tr><tr><td style="height:20px">休憩<br>16：10～60：20</td><td class="tenminyasumi"></td></tr><tr><td style="height:90px">9限<br>16：20～17：05</td><td rowspan="2" id="ninegen_extension_overlay"></td></tr><tr><td style="height:90px">10限<br>17：05～17：50</td></tr></table>',
+  // );
 }
 
 function updateTimeScheduleByTodoItem(todoItem, todoItemIndex) {
