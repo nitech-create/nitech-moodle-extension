adb860e29ebe605e0684da322c97786332e0514e
Tsukina-mochi <Tsukina7mochi@gmail.com>
2021-07-09 17:47:38 +0900

    collect styles

--- webpack.config.js
+++ webpack.config.js
@@ -46,10 +46,8 @@ module.exports = {
   },
   entry: {
     background: './src/background/backgroundEvent.js',
-    // calendar: './src/contents/calender/calender.js',
-    // videoArea: './src/contents/videoArea/videoArea.js',
-    main: './src/contents/general/main.js',
-    topPageStyle: './src/contents/top/topPageStyle.scss',
+    main: './src/contents/main.js',
+    topPageStyle: './src/contents/styles/top/topPageStyle.scss',
   },
   output: {
     filename: '[name].js',
