4555488eae76386b90f7afa7cc3eb7da0fe1beca
smk7758 <18097393+smk7758@users.noreply.github.com>
2021-12-28 23:56:21 +0900

    promiseWrapper、nullとは言わせない。

--- src/lib/promiseWrapper.js
+++ src/lib/promiseWrapper.js
@@ -1,77 +1,68 @@
 const promiseWrapper = {
   storage: {
     local: {
-      // wrapper of chrome.storage.local.get
-      get: null,
+      /**
+       * Wrapper of chrome.storage.local.get.
+       * storageにキーに対応するデータがないにErrorをthrowします。しっかりcatchしましょう。
+       * @param {String} key
+       * @return {Promise}
+       */
+      get: key => {
+        return async (resolve, reject) => {
+          chrome.storage.local.get(key, data => {
+            if (!data.hasOwnProperty(key)) {
+              // ストレージにキーが存在しない
+              reject(new Error('storage access by undefined key: ' + key));
+            } else {
+              resolve(data);
+            }
+          });
+        };
+      },
+      /**
+       * Wrapper of chrome.storage.local.get.
+       * storageにキーに対応するデータがないにErrorをthrowします。しっかりcatchしましょう。
+       * @param {String} key
+       * @return {Promise}
+       */
       // wrapper of chrome.storage.local.set
-      set: null,
+      set: data => {
+        return new Promise((resolve, reject) => {
+          chrome.storage.local.set(data, () => {
+            resolve(data);
+          });
+        });
+      },
     },
   },
   runtime: {
     // wrapper of chrome.runtime.sendMessage
-    sendMessage: null,
+    sendMessage: data => {
+      return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage(data, response => {
+          resolve(response);
+        });
+      });
+    },
   },
   tabs: {
     // wrapper of chrome.tabs.query
-    query: null,
+    query: options => {
+      return new Promise((resolve, reject) => {
+        chrome.tabs.query(options, tabs => {
+          resolve(tabs);
+        });
+      });
+    },
     // wrapper of chrome.tabs.sendMessage
-    sendMessage: null,
+    sendMessage: (tabId, data) => {
+      return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage(tabId, data, response => {
+          resolve(response);
+        });
+      });
+    },
   },
-};
-
-/**
- * Wrapper of chrome.storage.local.get.
- * storageにキーに対応するデータがないにErrorをthrowします。しっかりcatchしましょう。
- * @param {String} key
- * @return {Promise}
- */
-promiseWrapper.storage.local.get = key => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.local.get(key, data => {
-      if (!data.hasOwnProperty(key)) {
-        // ストレージにキーが存在しない
-        reject(new Error('storage access by undefined key: ' + key));
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-// wrapper of chrome.storage.local.set
-promiseWrapper.storage.local.set = data => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.local.set(data, () => {
-      resolve(data);
-    });
-  });
-};
-
-// wrapper of chrome.runtime.sendMessage
-promiseWrapper.runtime.sendMessage = data => {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage(data, response => {
-      resolve(response);
-    });
-  });
-};
-
-// wrapper of chrome.tabs.query
-promiseWrapper.tabs.query = options => {
-  return new Promise((resolve, reject) => {
-    chrome.tabs.query(options, tabs => {
-      resolve(tabs);
-    });
-  });
-};
-
-// wrapper of chrome.tabs.sendMessage
-promiseWrapper.tabs.sendMessage = (tabId, data) => {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage(tabId, data, response => {
-      resolve(response);
-    });
-  });
 };
 
 export default promiseWrapper;
