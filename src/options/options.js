751d6480831592971deaa20850a799f2abd6b144
smk7758 <18097393+smk7758@users.noreply.github.com>
2022-01-04 20:41:40 +0900

    関数名の変更と修正とリファクタリング。

--- src/options/options.js
+++ src/options/options.js
@@ -10,36 +10,55 @@ import $ from 'jQuery';
 })(); // PageLoad時に実行する。
 
 function generatePage(options) {
-  applyOptionsToPages(options);
+  applyPageFromOptions(options);
 
   console.log('generatePage: options: ', options);
   // saveボタン
   $('#btnSave').on('click', () => {
-    const options_ = {
-      backgroundColor: $('#backgroundColor').val(),
-      hideNavOnVideo: $('#hideNavOnVideo').prop('checked'),
-    };
-
-    chrome.runtime.sendMessage({ item: 'saveOptions', src: options_ });
+    applyOptionsFromPage(options);
+    optionsUtils.saveOptions(options);
+    // chrome.runtime.sendMessage({ item: 'saveOptions', src: options_ });
   });
 
   // loadDefaultボタン
-  $('#btnLoadDefault').on('click', () => {
-    chrome.runtime.sendMessage({ item: 'getDefaultOptions' }, response => {
-      applyOptionsToPages(response.getDefaultOptions); // response == options?
-    });
+  $('#btnLoadDefault').on('click', async () => {
+    const defaultOptions = await optionsUtils.getDefaultOptions();
+    applyPageFromOptions(defaultOptions);
+    // chrome.runtime.sendMessage({ item: 'getDefaultOptions' }, options => {
+    //   applyOptionsToPages(options); // response == options?
+    // });
   });
 
   // loadCurrentボタン
-  $('#btnLoadCurrent').on('click', () => {
-    chrome.runtime.sendMessage({ item: 'getOptions' }, response => {
-      applyOptionsToPages(response.options);
-    });
+  $('#btnLoadCurrent').on('click', async () => {
+    const options = await optionsUtils.getOptions();
+    applyPageFromOptions(options);
+    // chrome.runtime.sendMessage({ item: 'getOptions' }, options => {
+    //   applyOptionsToPages(options);
+    // });
   });
 }
 
-function applyOptionsToPages(options) {
-  console.log('applyOptionsToPages: ', options);
+/**
+ * optionsの要素に、optionsページの要素の値を代入します。
+ * see: applyPageFromOptions, 類似関数にapplyPageFromOptionsがあることに注意してください。
+ * @param {array} options
+ * @return {array} page内容が代入されたoptions
+ */
+function applyOptionsFromPage(options) {
+  console.log('applyOptionsFromPage: ', options);
+  options.backgroundColor = $('#backgroundColor').val();
+  options.hideNavOnVideo = $('#hideNavOnVideo').prop('checked');
+  return options;
+}
+
+/**
+ * optionsページに、引数のoptionsの要素の値を適用します。
+ * see: applyOptionsFromPage, 類似関数にapplyOptionsFromPageがあることに注意してください。
+ * @param {array} options
+ */
+function applyPageFromOptions(options) {
+  console.log('applyPageFromOptions: ', options);
   // ページに設定を反映
   $('#backgroundColor').val(options.backgroundColor);
   $('#hideNavOnVideo').prop('checked', options.hideNavOnVideo);
