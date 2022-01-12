371fe3eee7dd00e7ae59b073e99196a6d2d1fcc3
Your Name <18097393+smk7758@users.noreply.github.com>
2022-01-12 18:01:36 +0900

    loadFileの削除とtimeTableGraphicalの修正

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,51 +9,50 @@ export function injectScript(code) {
   script.remove();
 }
 
-export function loadJson(filePath, callback) {
-  // chrome.runtime.getPackageDirectoryEntry(function (root) {
-  //   // get file
-  //   root.getFile(filePath, { create: false }, function (sample) {
-  //     // callback
-  //     sample.file(function (file) {
-  //       // read file
-  //       const reader = new FileReader();
-  //       reader.readAsText(file);
-  //       reader.addEventListener('load', function (e) {
-  //         // parse and return json
-  //         // const response = {
-  //         //   url: chrome.extension.getURL('data'),
-  //         //   settings: JSON.parse(e.target.result),
-  //         // };
+// export function loadJson(filePath, callback) {
+//   // chrome.runtime.getPackageDirectoryEntry(function (root) {
+//   //   // get file
+//   //   root.getFile(filePath, { create: false }, function (sample) {
+//   //     // callback
+//   //     sample.file(function (file) {
+//   //       // read file
+//   //       const reader = new FileReader();
+//   //       reader.readAsText(file);
+//   //       reader.addEventListener('load', function (e) {
+//   //         // parse and return json
+//   //         // const response = {
+//   //         //   url: chrome.extension.getURL('data'),
+//   //         //   settings: JSON.parse(e.target.result),
+//   //         // };
 
-  //         const json = JSON.parse(e.target.result);
-  //         callback(json, chrome.extension.getURL('data'));
-  //       });
-  //     });
-  //   });
-  // });
+//   //         const json = JSON.parse(e.target.result);
+//   //         callback(json, chrome.extension.getURL('data'));
+//   //       });
+//   //     });
+//   //   });
+//   // });
 
-  // 上記コードに対して下を実装。未検証
-  loadFile(filePath, (result, url) => {
-    const json = JSON.parse(result);
-    callback(json, url);
-  });
-}
+//   // 上記コードに対して下を実装。未検証
+//   loadFile(filePath, (result, url) => {
+//     const json = JSON.parse(result);
+//     callback(json, url);
+//   });
+// }
 
-export function loadFile(filePath, callback) {
-  // 動かない？
-
-  chrome.runtime.getPackageDirectoryEntry(root => {
-    // get file
-    root.getFile(filePath, { create: false }, fileEntry => {
-      // callback
-      fileEntry.file(file => {
-        // read file
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.addEventListener('load', e => {
-          callback(e.target.result);
-        });
-      });
-    });
-  });
-}
+// export function loadFile(filePath, callback) {
+// 動かない(APIが消えたみたい)
+// chrome.runtime.getPackageDirectoryEntry(root => {
+//   // get file
+//   root.getFile(filePath, { create: false }, fileEntry => {
+//     // callback
+//     fileEntry.file(file => {
+//       // read file
+//       const reader = new FileReader();
+//       reader.readAsText(file);
+//       reader.addEventListener('load', e => {
+//         callback(e.target.result);
+//       });
+//     });
+//   });
+// });
+// }
