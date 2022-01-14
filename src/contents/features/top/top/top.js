92b1668dc053b7e9571f44572ccb95c0ad824969
smk7758 <18097393+smk7758@users.noreply.github.com>
2022-01-14 11:12:00 +0900

    a bit fix

--- src/contents/features/top/top/top.js
+++ src/contents/features/top/top/top.js
@@ -1,27 +1,30 @@
 import { injectScript } from 'Lib/utils.js';
 import $ from 'jQuery';
-import untilPageLoaded from './untilPageLoaded.js'
+import untilPageLoaded from './untilPageLoaded.js';
 
-const onTopPage = async function() {
+const onTopPage = async () => {
   // topページでの処理
   await untilPageLoaded();
 
   const courseValue = $('.coursename');
 
   // コース概要のフィルタを「すべて表示(表示から削除済みを除く)」にする
-  injectScript(`$('#groupingdropdown').next('.dropdown-menu').find('a[data-value="all"]').click();`);
+  injectScript(
+    `$('#groupingdropdown').next('.dropdown-menu').find('a[data-value="all"]').click();`,
+  );
   await untilPageLoaded();
 
   // コースの表示数を「すべて」にする
-  injectScript(`$('button[data-action="limit-toggle"]').next('.dropdown-menu').find('a[data-limit="0"]').click();`);
+  injectScript(
+    `$('button[data-action="limit-toggle"]').next('.dropdown-menu').find('a[data-limit="0"]').click();`,
+  );
   await untilPageLoaded();
 
   console.log('value: ', courseValue.length, courseValue);
-}
-
+};
 
 import config from './top.json5';
 export default {
   config,
-  func: onTopPage
-}
+  func: onTopPage,
+};
