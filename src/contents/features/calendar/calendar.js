0fad78e2ecfac61663f484f899cb6c37fead1ca6
smk7758 <18097393+smk7758@users.noreply.github.com>
2022-01-19 12:44:59 +0900

    bring dev

--- src/contents/features/calendar/calendar.js
+++ src/contents/features/calendar/calendar.js
@@ -1,51 +1,32 @@
-import $ from 'jquery';
 import promiseWrapper from 'Lib/promiseWrapper.js';
-import { isUndefined } from '../../../lib/utils';
+
 // calenderの選択ボタンは本来、コース名(授業番号)となってしまっているため、授業名に表示のみ差し替える機能。
-async function onLoad() {
+async function changeDisplayName() {
   const courses = await promiseWrapper.storage.local
     .get('courses')
     .then(result => {
       return result.courses;
     })
     .catch(err => {
-      console.log('[changeDisplayName] cannot load courses.');
+      console.log('[getCourses] cannot load old courses.');
       return undefined;
     });
 
-  changeDisplayName(courses);
-}
-
-async function changeDisplayName(courses) {
   const options = document.getElementById('menucourse').options;
-  console.log('[changeDisplayName] options: ', options);
-  Array.prototype.forEach.call(options, option => {
+  Array.prototype.forEach.call(options, function (option) {
     const course = courses.find(course => course.courseNumberTxt == option.label);
-
-    if (isUndefined(course)) {
+    if (course == undefined) {
       if (!(option.value == 1 && option.label == 'すべてのコース')) {
         console.log('Cannot find!: ' + option.label);
       }
-      return;
+    } else {
+      option.label = course.name + ' (' + course.courseNumberTxt + ')';
     }
-
-    option.label = course.name + ' (' + course.courseNumberTxt + ')';
-    console.log('[changeDisplayName] option.label: ', option.label);
-  });
-}
-
-async function changeSubmittedTask(courses) {
-  const events = $("#region-main li[data-region='event-item'] a");
-  events.each((_, e) => {
-    const url = new URL($(e).attr('href'));
-    const id = url.searchParams.get('id');
-    if (id && status[id])
-      if (status[id].isSubmitted) $(e).children('span:first').css('background-color', '#66cc66');
   });
 }
 
 import config from './calendar.json5';
 export default {
   config,
-  func: onLoad,
+  func: changeDisplayName,
 };
